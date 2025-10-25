c95f899a4c6e77868d6603fbc72cbc5c52f6609d
Om-Rohilla <omrohilla106@gmail.com>
2025-10-25 15:53:17 +0530

    Created the folder structure

---
+++ src/components/ui/component.tsx
@@ -0,0 +1,7 @@
+// Main component logic will go here
+// This file is required for 21st.dev registry compatibility
+
+export default function Component() {
+  return null;
+}
+
---
+++ src/demos/default.tsx
@@ -0,0 +1,7 @@
+// Visual demo/preview will go here
+// This file is required for 21st.dev registry compatibility
+
+export default function Demo() {
+  return null;
+}
+
